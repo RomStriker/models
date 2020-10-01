cedf5111b3179e0e72e8e299f6efe65c4ff8e1f1
Your name <romannaeemrn@gmail.com>
2020-10-01 19:53:41 +0200

    Update model_main.py

--- research/object_detection/model_main.py
+++ research/object_detection/model_main.py
@@ -61,7 +61,7 @@ def main(unused_argv):
   #os.environ["CUDA_VISIBLE_DEVICES"] = "1"
   flags.mark_flag_as_required('model_dir')
   flags.mark_flag_as_required('pipeline_config_path')
-  config = tf.estimator.RunConfig(model_dir=FLAGS.model_dir, save_checkpoints_steps=500, keep_checkpoint_max=500)
+  config = tf.estimator.RunConfig(model_dir=FLAGS.model_dir, save_checkpoints_steps=500, keep_checkpoint_max=5)
 
   train_and_eval_dict = model_lib.create_estimator_and_inputs(
       run_config=config,
